160cc0cc8017cf3191b1c39cbf22c644e5026624
Tiago Luchini <tiago@luchini.nyc>
2025-12-27 11:05:24 -0800

    Add colorful rainbow output to Hello World

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const colors = [
+  "\x1b[31m", // red
+  "\x1b[33m", // yellow
+  "\x1b[32m", // green
+  "\x1b[36m", // cyan
+  "\x1b[34m", // blue
+  "\x1b[35m", // magenta
+];
+const reset = "\x1b[0m";
+
+const text = "Hello World!";
+const colorfulText = text
+  .split("")
+  .map((char, i) => `${colors[i % colors.length]}${char}`)
+  .join("");
+
+console.log(colorfulText + reset);
